62887ae1de946dc23510c181a1cb6800c4a61d1f
Ivan Sevilla <ivansevillaa@gmail.com>
2019-03-19 13:44:26 -0300

    adding a store

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,6 +2,21 @@ import React from 'react'
 import { render } from 'react-dom'
 import HomeContainer from '../pages/Home/HomeContainer.jsx'
 import data from '../api.json'
+import { createStore } from 'redux'
+
+const initialState = {
+    data: {
+        ...data
+    }
+}
+
+const store = createStore(
+    (state) => state,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log(store.getState());
 
 const $homeContainer = document.getElementById('home-container')
 
