03a2fad0d1ee9c66660fa16f634f192abfb33947
Ivan Sevilla <ivansevillaa@gmail.com>
2019-03-19 13:54:27 -0300

    integrating the store with the Provider

--- src/entries/home.js
+++ src/entries/home.js
@@ -3,21 +3,24 @@ import { render } from 'react-dom'
 import HomeContainer from '../pages/Home/HomeContainer.jsx'
 import data from '../api.json'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 const initialState = {
     data: {
         ...data
     }
 }
-
 const store = createStore(
     (state) => state,
     initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-
 console.log(store.getState());
 
 const $homeContainer = document.getElementById('home-container')
-
-render(<HomeContainer data={data} />, $homeContainer)
+render(
+    <Provider store={store}>
+        <HomeContainer />
+    </Provider>,
+    $homeContainer
+)
