d524df85613bb652d086921093c9749ac82eee62
Ivan Sevilla <ivansevillaa@gmail.com>
2019-03-24 18:55:25 -0300

    middlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
 import HomeContainer from '../pages/Home/HomeContainer.jsx'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from '../reducers/index.js'
 import { Map as map} from 'immutable'
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+// function logger({ getState, dispatch}) {
+//     return (next) => {
+//         return (action) => {
+//             console.log('OLD STATE -> ', getState().toJS())
+//             console.log('ACION TO SEND -> ', action)
+//             const value = next(action)
+//             console.log('NEW STATE -> ', getState().toJS())
+//             return value
+//         }
+//     }
+// }
 
 const store = createStore(
     reducer,
     map(),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeWithDevTools (
+        applyMiddleware(logger)
+    )
 )
 
 const $homeContainer = document.getElementById('home-container')
